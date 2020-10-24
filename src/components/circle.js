2f369b8f12decfb81d18bd705b6f6f4bff3c7ada
Chander Parkash <30454653+cprajput@users.noreply.github.com>
2020-10-24 17:52:37 +0530

    Add files via upload

---
+++ src/components/circle.js
@@ -0,0 +1,17 @@
+// import using require
+
+// declare class
+
+
+// export class using module.exports
+
+
+class Circle extends Shape {
+    constructor(){
+        super();
+        this.calculateArea=()=>{
+            
+        }
+    }
+}
+export {Circle};
---
+++ src/components/shape.js
@@ -0,0 +1,16 @@
+// declare class
+
+// export class using module.exports
+
+class Shape {
+    constructor(){
+        this.color = "red";
+        this.drawShape  = ()=>{
+
+        }
+        this.calculateArea=()=>{
+
+        }
+    }
+}
+export {Shape};
